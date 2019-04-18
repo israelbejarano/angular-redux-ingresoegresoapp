3cc7b779b203dcfebd8077608eee67bb8296c353
israel <israel.bejarano.a@gmail.com>
2019-04-18 16:24:40 +0200

    crear usuario en firebase BBDD

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
 import * as firebase from 'firebase';
 import { map } from 'rxjs/operators';
+import { User } from './user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private afAuth: AngularFireAuth, private router: Router) { }
+  constructor(private afAuth: AngularFireAuth, private router: Router, private afBD: AngularFirestore) { }
 
   initAuthListener() {
     this.afAuth.authState.subscribe((fbUser: firebase.User) => {
@@ -23,7 +25,17 @@ export class AuthService {
     this.afAuth.auth.createUserWithEmailAndPassword(email, password)
     .then(resp => {
       console.log(resp);
-      this.router.navigate(['/']);
+      const user: User = {
+        uid: resp.user.uid,
+        nombre: nombre,
+        email: resp.user.email
+      };
+      this.afBD.doc(`${user.uid}/usuario`).set(user).then(() => {
+        this.router.navigate(['/']);
+      }).catch(error => {
+        console.error(error);
+        Swal.fire('Error guardando en Firebase BBDD', error.message, 'error');
+      });
     }).catch(error => {
       console.error(error);
       Swal.fire('Error en el registro', error.message, 'error');
