777bb8017cf4d145eda6a7fc39f2eeca0a91ce9b
israel <israel.bejarano.a@gmail.com>
2019-04-19 17:47:12 +0200

    uso del servicio para crear ingresos egresos

--- src/app/ingreso-egreso/ingreso-egreso.component.ts
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { IngresoEgreso } from './ingreso-egreso.model';
+import { IngresoEgresoService } from './ingreso-egreso.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-ingreso-egreso',
@@ -12,7 +14,7 @@ export class IngresoEgresoComponent implements OnInit {
   forma: FormGroup;
   tipo = 'ingreso';
 
-  constructor() { }
+  constructor(public ingresoEgresoService: IngresoEgresoService) { }
 
   ngOnInit() {
     this.forma = new FormGroup({
@@ -24,6 +26,14 @@ export class IngresoEgresoComponent implements OnInit {
   crearIngresoEgreso() {
     const ingresoEgreso = new IngresoEgreso({...this.forma.value, tipo: this.tipo});
     console.log(ingresoEgreso);
+    this.ingresoEgresoService.crearIngresoEgreso(ingresoEgreso)
+    .then(() => {
+      this.forma.reset({monto: 0});
+      Swal.fire('Creado', ingresoEgreso.descripcion, 'success');
+    })
+    .catch(error => {
+      console.error(error)
+    });
   }
 
 }
