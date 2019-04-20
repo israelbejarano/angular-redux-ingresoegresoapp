a607c2068c3639fa359a881b71dffaa3eaa16d31
israel <israel.bejarano.a@gmail.com>
2019-04-20 12:17:58 +0200

    get colecciones de firebase + save en store

--- src/app/ingreso-egreso/ingreso-egreso.service.ts
+++ src/app/ingreso-egreso/ingreso-egreso.service.ts
@@ -4,24 +4,37 @@ import { IngresoEgreso } from './ingreso-egreso.model';
 import { AuthService } from '../auth/auth.service';
 import { Store } from '@ngrx/store';
 import { AppState } from '../app.reducer';
-import { filter } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
+import { SetItemsAction } from './ingreso-egreso.actions';
+import { Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IngresoEgresoService {
 
+  ingresoEgresoListenerSubscription: Subscription = new Subscription();
+  ingresoEgresoItemsListenerSubscription: Subscription = new Subscription();
+
   constructor(private afBD: AngularFirestore, public authService: AuthService, private store: Store<AppState>) { }
 
   initIngresoEgresoListener() {
-    this.store.select('auth').pipe(filter(auth => auth.user != null))
+    this.ingresoEgresoListenerSubscription = this.store.select('auth').pipe(filter(auth => auth.user != null))
       .subscribe(auth => this.ingresoEgresoItems(auth.user.uid));
   }
 
   private ingresoEgresoItems(uid: string) {
-    this.afBD.collection(`${uid}/ingresos-egresos/items`)
-      .valueChanges().subscribe(docData => {
-        console.log(docData);
+    this.ingresoEgresoItemsListenerSubscription = this.afBD.collection(`${uid}/ingresos-egresos/items`)
+      .snapshotChanges().pipe(map(docData => {
+        return docData.map(doc => {
+          return {
+            uid: doc.payload.doc.id,
+            ...doc.payload.doc.data()
+          };
+        });
+      })).subscribe((coleccion: any[]) => {
+        console.log(coleccion);
+        this.store.dispatch(new SetItemsAction(coleccion));
       });
   }
 
@@ -29,5 +42,10 @@ export class IngresoEgresoService {
     const usuario = this.authService.getUsuario();
     return this.afBD.doc(`${usuario.uid}/ingresos-egresos`).collection('items')
       .add({...ingresoEgreso});
+  }
+
+  cancelarSubscription() {
+    this.ingresoEgresoItemsListenerSubscription.unsubscribe();
+    this.ingresoEgresoListenerSubscription.unsubscribe();
   }
 }
