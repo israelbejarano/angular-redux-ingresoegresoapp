301b418b9928fa9b7108cf3f62d974ed8ec4cfec
israel <israel.bejarano.a@gmail.com>
2019-04-20 16:30:09 +0200

    mostrar nombre del usuario

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
+import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styles: []
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnDestroy {
 
-  constructor(public authService: AuthService) { }
+  nombre: string;
+  subscription: Subscription = new Subscription();
+
+  constructor(public authService: AuthService, private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.subscription = this.store.select('auth')
+      .pipe(filter(auth => auth.user != null))
+      .subscribe(auth => this.nombre = auth.user.nombre);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   logout() {
